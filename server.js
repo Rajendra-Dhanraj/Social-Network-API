cd3ca2390937fd2f6a506c1c23490bfcf410cd3e
Rajendra Dhanraj <rajiedhan@Rajendras-MacBook-Pro.local>
2021-04-06 23:04:14 -0400

    created directories,and server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose"); // connects to mongoose when app is started
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(require("./routes"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network", { //  tells Mongoose which database we want to connect to. 
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
